8425cecc88b4787c0a751ae6bb8d1c8dc174649f
Javier Pérez <javier.perez@davinchi.es>
2018-05-14 15:10:00 +0200

    chore: Release 1.0.0

---
+++ esm2015/jquery-ui-deps.js
@@ -0,0 +1,13 @@
+/**
+ * Required jquery ui dependencies
+ */
+import "jquery-ui/ui/version";
+import "jquery-ui/ui/plugin";
+import "jquery-ui/ui/position";
+import "jquery-ui/ui/data";
+import "jquery-ui/ui/scroll-parent";
+import "jquery-ui/ui/widget";
+import "jquery-ui/ui/safe-active-element";
+import "jquery-ui/ui/safe-blur";
+import "jquery-ui/ui/widgets/draggable";
+import "jquery-ui/ui/widgets/dialog";
